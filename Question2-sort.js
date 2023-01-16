8c280cf582bedc283f88f9e2084528ab49a7c6fb
Chetan Satpute <chetan.satpute2002@gmail.com>
2023-01-16 18:03:19 +0530

    Correction in sort method of question 2

--- Question2-sort.js
+++ Question2-sort.js
@@ -1,8 +1,8 @@
 // prettier-ignore
-const data = [ { id: 1, first_name: "Nicki", email: "ncrozier0@squarespace.com", date_of_birth: "2009/05/09", }, { id: 2, first_name: "Raychel", email: "rmcgrady1@cpanel.net", date_of_birth: "1996/11/05", }, ];
+const data = [ { id: 2, first_name: "Raychel", email: "rmcgrady1@cpanel.net", date_of_birth: "1996/11/05", }, { id: 1, first_name: "Nicki", email: "ncrozier0@squarespace.com", date_of_birth: "2009/05/09", }, ];
 
 // =================================
-// sort method is non-mutating method
+// sort method is mutating method
 
 console.log(data);
 /*
@@ -10,16 +10,16 @@ console.log(data);
 Output:
 [
   {
-    id: 1,
-    first_name: 'Nicki',
-    email: 'ncrozier0@squarespace.com',
-    date_of_birth: '2009/05/09'
-  },
-  {
     id: 2,
     first_name: 'Raychel',
     email: 'rmcgrady1@cpanel.net',
     date_of_birth: '1996/11/05'
+  },
+  {
+    id: 1,
+    first_name: 'Nicki',
+    email: 'ncrozier0@squarespace.com',
+    date_of_birth: '2009/05/09'
   }
 ]
 
