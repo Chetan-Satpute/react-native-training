735fa7c81f39e1cece85138845a556d44aac6b7e
Chetan Satpute <chetan.satpute2002@gmail.com>
2023-01-23 12:06:57 +0530

    Getting Started with React

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import StateExample from "./components/StateExample";
+import StatelessExample from "./components/StatelessExample";
+
+function App() {
+  return (
+    <div>
+      <StateExample />
+      <StatelessExample />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/StatelessExample.jsx
@@ -0,0 +1,21 @@
+function StatelessExample() {
+    // Example of stateless component
+    return (
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+            </tr>
+            <tr>
+                <th>Football</th>
+                <th>$49.99</th>
+            </tr>
+            <tr>
+                <th>Baseball</th>
+                <th>$9.99</th>
+            </tr>
+        </table>
+    )
+}
+
+export default StatelessExample;
