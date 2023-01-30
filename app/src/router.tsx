709bc34910819369021990bbd171b0c1ca98c985
Chetan Satpute <chetan.satpute2002@gmail.com>
2023-01-30 10:16:21 +0530

    Changed from hash router to browser router

--- app/src/router.tsx
+++ app/src/router.tsx
@@ -1,9 +1,9 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Animation from "./routes/Animation";
 import HOC from "./routes/HOC";
 import Home from "./routes/Home";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   { path: "/", element:  <Home /> },
   { path: "/hoc", element:  <HOC /> },
   { path: "/animation", element:  <Animation /> },
